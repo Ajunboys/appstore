4bc052325fb459b0dc025e5893983572eee29ac0
Bernhard Posselt <dev@bernhard-posselt.com>
2017-08-03 12:12:17 +0200

    update app templates

--- nextcloudappstore/core/scaffolding/app-templates/12/app/tests/Integration/AppTest.php
+++ nextcloudappstore/core/scaffolding/app-templates/12/app/tests/Integration/AppTest.php
@@ -3,7 +3,9 @@
 namespace OCA\{{ app.namespace }}\Tests\Integration\Controller;
 
 use OCP\AppFramework\App;
-use Test\TestCase;
+use OCP\App\IAppManager;
+
+use PHPUnit_Framework_TestCase;
 
 
 /**
@@ -11,18 +13,17 @@ use Test\TestCase;
  * directly from the container, only pass in mocks if needed and run your tests
  * against the database
  */
-class AppTest extends TestCase {
+class AppTest extends PHPUnit_Framework_TestCase {
 
     private $container;
 
     public function setUp() {
-        parent::setUp();
         $app = new App('{{ app.id }}');
         $this->container = $app->getContainer();
     }
 
     public function testAppInstalled() {
-        $appManager = $this->container->query('OCP\App\IAppManager');
+        $appManager = $this->container->query(IAppManager::class);
         $this->assertTrue($appManager->isInstalled('{{ app.id }}'));
     }
 
